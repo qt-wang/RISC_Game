2495b7e2782ab975e46b8852a72d2a522f2fef73
Pzi0 <sggfu.rick@gmail.com>
2021-03-12 11:37:32 +0800

    Add army defination

--- server/src/main/java/edu/duke/ece651_g10/server/Territory.java
+++ server/src/main/java/edu/duke/ece651_g10/server/Territory.java
@@ -11,8 +11,8 @@ import java.util.List;
 public class Territory {
     private String name;
     private Player owner;
-    private HashMap<Player, List<Unit>> units;
+    private HashMap<Player, Army> units;
     private HashSet<Territory> neighbours;
     // This units are all owned by the owner of the territory.
-    private List<Unit> ownedUnits;
+    private Army ownedUnits;
 }
---
+++ server/src/main/java/edu/duke/ece651_g10/server/V1Army.java
@@ -0,0 +1,8 @@
+package edu.duke.ece651_g10.server;
+
+/**
+ * A class that implements the Army interface.
+ * Maintained by Guancheng Fu
+ */
+public class V1Army {
+}
