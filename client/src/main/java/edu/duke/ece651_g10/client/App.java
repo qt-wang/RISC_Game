c78f1ad7ab9db8b751710f41b7e1e7642ace2a59
Pzi0 <sggfu.rick@gmail.com>
2021-04-04 15:41:26 +0800

    temp state

--- client/src/main/java/edu/duke/ece651_g10/client/App.java
+++ client/src/main/java/edu/duke/ece651_g10/client/App.java
@@ -4,41 +4,47 @@
 package edu.duke.ece651_g10.client;
 
 import java.io.BufferedReader;
+
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 /**
  * The App class
  */
-public class App {
-  final Client client;
-  final SocketClient socketClient;
+public class App extends Application {
+    final Client client;
+    final SocketClient socketClient;
 
-  /**
-   * The constructor of the App class
-   *
-   * @param input    The BufferedReader input
-   * @param hostname The hostname of the game
-   * @param port     The port of the game
-   */
-  public App(BufferedReader input, String hostname, int port) throws IOException {
-    this.socketClient = new SocketClient(hostname, port);
-    this.client = new Client(System.out, input, socketClient);
-  }
-
-  /**
-   * The main function of the App
-   *
-   * @param args The argument of when user run the client program in the shell
-   */
-  public static void main(String[] args) throws IOException {
-    BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-    App app = new App(input, "127.0.0.1", 12345);
-    while (true) {
-      app.client.setCurrentJSON(app.socketClient.receive());
-      app.client.commandMap.get(app.client.getCurrentMessageType()).run();
+    /**
+     * The constructor of the App class
+     *
+     * @param input    The BufferedReader input
+     * @param hostname The hostname of the game
+     * @param port     The port of the game
+     */
+    public App(BufferedReader input, String hostname, int port) throws IOException {
+        this.socketClient = new SocketClient(hostname, port);
+        this.client = new Client(System.out, input, socketClient);
     }
-  }
+
+    /**
+     * The main function of the App
+     *
+     * @param args The argument of when user run the client program in the shell
+     */
+    public static void main(String[] args) throws IOException {
+//    BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+//    App app = new App(input, "127.0.0.1", 12345);
+//    while (true) {
+//      app.client.setCurrentJSON(app.socketClient.receive());
+//      app.client.commandMap.get(app.client.getCurrentMessageType()).run();
+//    }
+        launch(args);
+    }
+
+    public void start(Stage primaryStage) throws IOException {
+        primaryStage.show();
+    }
 }
 
 
