abd4ffe26c12e3b612722917af74dbdf77d955f4
Pzi0 <sggfu.rick@gmail.com>
2021-04-06 00:25:59 +0800

    A state that can pass tests

--- client/src/main/java/edu/duke/ece651_g10/client/MyAppWrapper.java
+++
@@ -1,9 +0,0 @@
-package edu.duke.ece651_g10.client;
-
-import javafx.application.Application;
-
-public class MyAppWrapper {
-    public static void main(String[] args) {
-        Application.launch(App.class, args);
-    }
-}
