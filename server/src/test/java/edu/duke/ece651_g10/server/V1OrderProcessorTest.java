453145cf775f5388bd254ddb601e3f91efe14097
qw99 <qiutong.wang@duke.edu>
2021-04-07 16:26:16 +0800

    add some tests to processor

--- server/src/test/java/edu/duke/ece651_g10/server/V1OrderProcessorTest.java
+++ server/src/test/java/edu/duke/ece651_g10/server/V1OrderProcessorTest.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class V1OrderProcessorTest {
-  @Disabled
+  @Test
   public void test_acceptOrder() {
     FixedGameMapFactory factory = new FixedGameMapFactory();
     GameMap gMap = factory.createGameMap(3);
@@ -30,8 +30,8 @@ public class V1OrderProcessorTest {
     assertEquals(7, gMap.getTerritory("Roshar").getNumUnit());
     assertEquals(4, gMap.getTerritory("Roshar").getArmyWithLevel(6).getArmyUnits());
     assertEquals(3, gMap.getTerritory("Roshar").getArmyWithLevel(0).getArmyUnits());
-    assertEquals(1, gMap.getTerritory("Narnia").getNumUnit());
-    assertEquals(1, gMap.getTerritory("Scadrial").getNumUnit());
+    assertEquals(1, gMap.getTerritory("Narnia").getArmyWithLevel(0).getArmyUnits());
+    assertEquals(1, gMap.getTerritory("Scadrial").getArmyWithLevel(0).getArmyUnits());
     assertEquals(0, gMap.getTerritory("Elantris").getNumUnit());
 
   }
@@ -63,11 +63,16 @@ public class V1OrderProcessorTest {
     gMap.getTerritory("Roshar").increaseUnit(6, 1);
     gMap.getTerritory("Roshar").increaseUnit(3, 4);
     AttackOrder aOrder1 = new AttackOrder(1, "Elantris", "Scadrial", 2, gMap, player, 0);
-    AttackOrder aOrder2 = new AttackOrder(1, "Roshar", "Scadrial", 1, gMap, player, 3);
+    AttackOrder aOrder2 = new AttackOrder(1, "Roshar", "Scadrial", 1, gMap, player, 1);
     V1OrderProcessor op = new V1OrderProcessor();
     op.acceptOrder(aOrder1);
     op.acceptOrder(aOrder2);
     op.executeEndTurnOrders();
+    assertEquals(3, gMap.getTerritory("Elantris").getArmyWithLevel(0).getArmyUnits());
+    assertEquals(5, gMap.getTerritory("Roshar").getArmyWithLevel(1).getArmyUnits());
+    assertEquals(2, gMap.getTerritory("Scadrial").getArmyWithLevel(0).getArmyUnits());
+    assertEquals(1, gMap.getTerritory("Scadrial").getArmyWithLevel(1).getArmyUnits());
+    assertEquals(0, gMap.getTerritory("Roshar").getArmyWithLevel(3).getArmyUnits());
     assertEquals(3, gMap.getTerritory("Scadrial").getNumUnit());
   }
 
@@ -89,6 +94,30 @@ public class V1OrderProcessorTest {
     op.executeEndTurnOrders();
     assertEquals(player2.getPlayerID(), gMap.getTerritory("Elantris").getOwner().getPlayerID());
     assertEquals(player1.getPlayerID(), gMap.getTerritory("Narnia").getOwner().getPlayerID());
+  }
+
+  @Test
+  public void test_attack(){
+    FixedGameMapFactory factory = new FixedGameMapFactory();
+    GameMap gMap = factory.createGameMap(3);
+    Player player = new Player(null, null);
+    gMap.getTerritory("Elantris").increaseUnit(5, 0);
+    gMap.getTerritory("Elantris").increaseUnit(2, 3);
+    gMap.getTerritory("Roshar").increaseUnit(6, 1);
+    gMap.getTerritory("Roshar").increaseUnit(3, 4);
+    gMap.getTerritory("Scadrial").increaseUnit(5, 1);
+    MoveOrder mOrder = new MoveOrder(1, "Elantris", "Roshar", 3, gMap, player, 0);
+    AttackOrder aOrder1 = new AttackOrder(1, "Elantris", "Scadrial", 2, gMap, player, 0);
+    AttackOrder aOrder2 = new AttackOrder(1, "Roshar", "Scadrial", 1, gMap, player, 1);
+    V1OrderProcessor op = new V1OrderProcessor();
+    op.acceptOrder(mOrder);
+    assertEquals(2, gMap.getTerritory("Elantris").getArmyWithLevel(0).getArmyUnits());
+    op.acceptOrder(aOrder1);
+    op.acceptOrder(aOrder2);
+    ArrayList<Integer> arr1 = new ArrayList<>(Arrays.asList(2, 1, 0, 0, 0, 0, 0));
+    assertEquals(arr1, aOrder1.getAttackLevel());
+    op.executeEndTurnOrders();
+    //assertEquals(3, gMap.getTerritory("Scadrial").getNumUnit());
   }
 }
 
