704ae6b5e2f8b0bf7b4c4f1e602d1c80d121c519
Xueyang Liu <xueyang.liu@qq.com>
2021-04-08 10:41:27 +0800

    removed Game.java, add documentations to ingamecontroller and gameinfo

--- client/src/main/java/edu/duke/ece651_g10/client/model/GameInfo.java
+++ client/src/main/java/edu/duke/ece651_g10/client/model/GameInfo.java
@@ -10,19 +10,56 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class GameInfo {
+    //the list of string to show in the listview of playerInfo
     List<String> playerInfo;
+    //current phase of game
+    //could be "Placement","Attack","GameEnd"
     String sub;
+    //true if the player can upgrade tech level, false if not
     boolean canUpgrade;
+    //the key is the terr's name, value is a list of string to show in the listview of terrInfo
     HashMap<String,List<String>> territoryInfos;
+    //key is all possible player id, value is the list of the terrs' names owned by that player
     HashMap<Integer, List<String>> territoryOwnerShip = new HashMap<>();
     // Have 3 player status A L E
     String playerStatus;
     //HashMap<Integer, String> colorStrategy = new HashMap<>();
 
+    /**
+     * construct the model by the JSONObject received
+     * set territory's infos and player infos
+     * @param received the JSONObject received by the client
+     */
+    public GameInfo(JSONObject received) {
+        //JSONObject
+        //received = generateTestJSON();
+        //System.out.println(received);
+        sub = received.getString("sub");
+        canUpgrade = received.getBoolean("canUpgrade");
+        playerStatus = received.getString("playerStatus");
+        //int playerNumber = received.getInt("playerNumber");
+        //setColorStrategy(playerNumber);
+        setTerritoryInfos(received);
+        if (playerStatus.equals("L")) {
+            setPlayerInfoLose(received);
+        } else {
+            setPlayerInfo(received);
+        }
+    }
+
+    /**
+     * get the player's status
+     * "A" or "L" or "E"
+     * @return
+     */
     public String getPlayerStatus() {
         return playerStatus;
     }
 
+    /**
+     * set the info when the player loses
+     * @param obj the received jsonObject
+     */
     public void setPlayerInfoLose(JSONObject obj) {
         try {
             List<String> info = new LinkedList<>();
@@ -34,24 +71,10 @@ public class GameInfo {
             e.printStackTrace();
         }
     }
-
-    public GameInfo(JSONObject received) {
-        //JSONObject
-        //received = generateTestJSON();
-        //System.out.println(received);
-        sub = received.getString("sub");
-        canUpgrade = received.getBoolean("canUpgrade");
-        playerStatus = received.getString("playerStatus");
-        int playerNumber = received.getInt("playerNumber");
-        //setColorStrategy(playerNumber);
-        setTerritoryInfos(received);
-        if (playerStatus.equals("L")) {
-            setPlayerInfoLose(received);
-        } else {
-            setPlayerInfo(received);
-        }
-    }
-
+    /**
+     * set the info when the player is alive
+     * @param obj the received jsonObject
+     */
     public void setPlayerInfo(JSONObject obj){
         try{
             List<String> info = new LinkedList<>();
@@ -83,9 +106,11 @@ public class GameInfo {
         }
     }
 
+
     public String getSub(){
         return sub;
     }
+
     public boolean getCanUpgrade(){
         return canUpgrade;
     }
@@ -103,6 +128,12 @@ public class GameInfo {
 //            colorStrategy.put(5,"Purple");
 //        }
 //    }
+
+    /**
+     * set the terr related infos according to the given json
+     * ownership, terrinfo
+     * @param obj the given jsonobject
+     */
     public void setTerritoryInfos(JSONObject obj){
         try{
             HashMap<String,List<String>> infos = new HashMap<>();
@@ -136,47 +167,47 @@ public class GameInfo {
             e.printStackTrace();
         }
     }
+
+    /**
+     * get the terr's info to show
+     * @param tName the required terr's name
+     * @return a list of string for controller to show in listview
+     */
     public List<String> getTerritoryInfo(String tName){
         return territoryInfos.getOrDefault(tName,null);
     }
 
+    /**
+     * get the playerinfo to show
+     * @return a list of string for controller to show in listview
+     */
     public List<String> getPlayerInfo(){
         return playerInfo;
     }
 
-    public void updateGameInfo(JSONObject obj){
-
-    }
-
-    public JSONObject generateTestJSON(){
-        JSONObject ans = new JSONObject();
-        ans.put("type","Game").put("sub","Attack").put("playerId",1);
-        ans.put("prompt","valid\n").put("reason","").put("playerStatus","A")
-                .put("foodResource",100).put("technologyResource",100)
-                .put("technologyLevel",2).put("canUpgrade",true).put("playerNumber",5);
-
-        JSONObject tInfo = new JSONObject();
-        for(int i=0;i<15;i++){
-            JSONObject singleT = new JSONObject();
-            JSONObject armies = new JSONObject();
-            for(int j = 0;j<7;j++){
-                String curr = ""+(char)('0'+j);
-                armies.put(curr,2);
-            }
-            singleT.put("armies",armies);
-            singleT.put("owner",i%5+1);
-            singleT.put("foodResourceGenerationRate",1);
-            singleT.put("technologyResourceGenerationRate",2);
-            String name = ""+(char)('A'+i);
-            tInfo.put(name,singleT);
-        }
-        ans.put("TerritoriesInformation",tInfo);
-        return ans;
-    }
-//    public GameInfo(JSONObject obj){
-//        playerInfo = new ArrayList<>();
-//        territoryInfos = new HashMap<>();
-//        //setPlayerInfo();
+//    public JSONObject generateTestJSON(){
+//        JSONObject ans = new JSONObject();
+//        ans.put("type","Game").put("sub","Attack").put("playerId",1);
+//        ans.put("prompt","valid\n").put("reason","").put("playerStatus","A")
+//                .put("foodResource",100).put("technologyResource",100)
+//                .put("technologyLevel",2).put("canUpgrade",true).put("playerNumber",5);
+//
+//        JSONObject tInfo = new JSONObject();
+//        for(int i=0;i<15;i++){
+//            JSONObject singleT = new JSONObject();
+//            JSONObject armies = new JSONObject();
+//            for(int j = 0;j<7;j++){
+//                String curr = ""+(char)('0'+j);
+//                armies.put(curr,2);
+//            }
+//            singleT.put("armies",armies);
+//            singleT.put("owner",i%5+1);
+//            singleT.put("foodResourceGenerationRate",1);
+//            singleT.put("technologyResourceGenerationRate",2);
+//            String name = ""+(char)('A'+i);
+//            tInfo.put(name,singleT);
+//        }
+//        ans.put("TerritoriesInformation",tInfo);
+//        return ans;
 //    }
-
 }
