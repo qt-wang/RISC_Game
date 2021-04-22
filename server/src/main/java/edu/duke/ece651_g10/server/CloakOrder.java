d0f120fbb22800bf7f3fa1d7f325b17bc5b5c084
qw99 <qiutong.wang@duke.edu>
2021-04-22 10:15:12 +0800

    m

--- server/src/main/java/edu/duke/ece651_g10/server/CloakOrder.java
+++ server/src/main/java/edu/duke/ece651_g10/server/CloakOrder.java
@@ -1,8 +1,8 @@
 package edu.duke.ece651_g10.server;
 
 public class CloakOrder extends OneTerritoryOrder{
-    public CloakOrder(int playerID, String source, int unitNum, GameMap gMap, Player player){
-        super(playerID, source, unitNum, gMap, 0, player);
+    public CloakOrder(int playerID, String source, GameMap gMap, Player player){
+        super(playerID, source, 0, gMap, 0, player);
     }
 
     public void execute(){
