8cab08222339c7a9252c6c4bf0b87a708eaff95f
qw99 <qiutong.wang@duke.edu>
2021-04-22 22:16:28 +0800

    add checkers and do some changes to orders

--- server/src/main/java/edu/duke/ece651_g10/server/VirusOrder.java
+++ server/src/main/java/edu/duke/ece651_g10/server/VirusOrder.java
@@ -1,10 +1,10 @@
 package edu.duke.ece651_g10.server;
 
-public class VirusOrder extends ZeroTerritoryOrder{
+public class VirusOrder extends OneTerritoryOrder{
     Player targetPlayer;
     int virusLevel;
     public VirusOrder(int playerID, GameMap gMap, Player player, String territory, int vLevel){
-        super(playerID, gMap, player);
+        super(playerID, territory, 0, gMap, 0, player);
         this.targetPlayer = gMap.getTerritory(territory).getOwner();
         this.virusLevel = vLevel;
     }
@@ -25,5 +25,37 @@ public class VirusOrder extends ZeroTerritoryOrder{
             }
             player.setTechnologyResourceTotal(player.getTechnologyResourceTotal() - 200); // One virus attack costs 100 tech resources
         }
+    }
+
+    /**
+     *
+     * @return the source territory
+     */
+    public Territory getSourceTerritory() {
+        return source;
+    }
+
+    /**
+     *
+     * @return the unit number
+     */
+    public int getNumUnit() {
+        return unitNum;
+    }
+
+    /**
+     *
+     * @return the the level that a player want to upgrade
+     */
+    public int getLevel(){
+        return level;
+    }
+
+    /**
+     *
+     * @return return the technology cost that this order needs
+     */
+    public int getTechnologyCost(){
+        return unitUpgradeTable.get(level + 1);
     }
 }
