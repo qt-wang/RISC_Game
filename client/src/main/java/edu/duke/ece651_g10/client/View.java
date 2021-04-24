31ddce5622cb307eb1743a782252b10dd7998c2b
Pzi0 <sggfu.rick@gmail.com>
2021-04-24 10:54:57 +0800

    First submission

--- client/src/main/java/edu/duke/ece651_g10/client/View.java
+++
@@ -1,5 +0,0 @@
-package edu.duke.ece651_g10.client;
-
-public interface View {
-
-}
--- client/src/test/java/edu/duke/ece651_g10/client/SceneFactoryTest.java
+++
@@ -1,7 +0,0 @@
-package edu.duke.ece651_g10.client;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class SceneFactoryTest {
-    
-}
